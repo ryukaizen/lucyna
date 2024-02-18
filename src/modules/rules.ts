f934abddad1bf28110c98379d82517873aefa0ff
ryukaizen <ryukaizen@protonmail.com>
2024-02-18 19:22:02 +0530

    add setrules command

--- src/modules/rules.ts
+++ src/modules/rules.ts
@@ -8,10 +8,10 @@ bot.chatType("supergroup" || "group").command("rules", (async(ctx: any) => {
     let rules_button = new InlineKeyboard()
         .url("Rules❕", `https://telegram.me/${constants.BOT_USERNAME}?start=rules_${chatId}`);  
     if (ctx.message.reply_to_message == undefined) {
-        await ctx.reply("Read the group rules from the button below.", {reply_markup: rules_button, reply_parameters: {message_id: ctx.message.message_id, parse_mode: "HTML" }});
+        await ctx.reply("Read the group rules via button below.", {reply_markup: rules_button, reply_parameters: {message_id: ctx.message.message_id}, parse_mode: "HTML"});
     }
     else {
-        await ctx.reply("Read the group rules from the button below.", {reply_markup: rules_button, reply_parameters: {message_id: ctx.message.reply_to_message.message_id}, parse_mode: "HTML" });
+        await ctx.reply("Read the group rules via button below.", {reply_markup: rules_button, reply_parameters: {message_id: ctx.message.reply_to_message.message_id}, parse_mode: "HTML" });
     }
 }));
 
@@ -23,3 +23,52 @@ export async function get_rules(chatId: string) {
     });
     return rules?.rules;
 }
+
+bot.chatType("supergroup" || "group").command("setrules", (async(ctx: any) => {
+    // only let this happen if the user using /setrules command is an admin of the chat
+    let user = await ctx.getAuthor();
+    if (user.status != "creator" && user.status != "administrator") {
+        await ctx.reply("Only admins can use this command.");
+    }
+    else {
+        let chatId = ctx.chat.id.toString();
+        // if the content is empty after /setrules command, tell the user to type rules next to /setrules command or reply to a message with /setrules command
+        if (ctx.message.text.replace("/setrules" || "!setrules" || "?setrules", "").trim() == "") {
+            // if the message is a reply to another message
+            if (ctx.message.reply_to_message != undefined) {
+                await prisma.rules.upsert({
+                    where: {
+                        chat_id: chatId,
+                    },
+                    update: {
+                        rules: ctx.message.reply_to_message.text,
+                    },
+                    create: {
+                        chat_id: chatId,
+                        rules: ctx.message.reply_to_message.text,
+                    },
+            });
+            await ctx.reply("Rules have been set.");
+            }
+            else {
+                await ctx.reply("Please type the rules next to /setrules command or reply to a message with /setrules command.", {reply_parameters: {message_id: ctx.message.message_id}});
+                return;
+            }
+        }
+        else {
+            await prisma.rules.upsert({
+                where: {
+                    chat_id: chatId,
+                },
+                update: {
+                    rules: ctx.message.text.replace("/setrules" || "!setrules" || "?setrules", "").trim(),
+                },
+                create: {
+                    chat_id: chatId,
+                    rules: ctx.message.text.replace("/setrules" || "!setrules" || "?setrules", "").trim(),
+                },
+            });
+            await ctx.reply("Rules have been set.", {reply_parameters: {message_id: ctx.message.message_id}});
+        }
+    }
+}));
