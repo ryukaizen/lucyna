26c50d8eeab28a80bcfe2ab003e7c3b36220cbd8
ryukaizen <ryukaizen@protonmail.com>
2024-02-18 18:02:25 +0530

    refactor rules command to handle undefined reply_to_message

--- src/modules/rules.ts
+++ src/modules/rules.ts
@@ -7,8 +7,12 @@ bot.chatType("supergroup" || "group").command("rules", (async(ctx: any) => {
     let chatId = ctx.chat.id.toString();
     let rules_button = new InlineKeyboard()
         .url("Rules❕", `https://telegram.me/${constants.BOT_USERNAME}?start=rules_${chatId}`);  
-    await ctx.reply("Read the group rules from the button below.", {reply_markup: rules_button, reply_parameters: {message_id: ctx.message.reply_to_message.message_id}, parse_mode: "HTML" });
-
+    if (ctx.message.reply_to_message == undefined) {
+        await ctx.reply("Read the group rules from the button below.", {reply_markup: rules_button, reply_parameters: {message_id: ctx.message.message_id, parse_mode: "HTML" }});
+    }
+    else {
+        await ctx.reply("Read the group rules from the button below.", {reply_markup: rules_button, reply_parameters: {message_id: ctx.message.reply_to_message.message_id}, parse_mode: "HTML" });
+    }
 }));
 
 export async function get_rules(chatId: string) {
