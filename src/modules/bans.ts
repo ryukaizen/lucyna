54e37c3ed9363bc9f2cd75b296bcd827b5aa1ebe
ryukaizen <ryukaizen@protonmail.com>
2024-02-21 16:23:23 +0530

    catch exceptions (needs improvement)

--- src/modules/bans.ts
+++ src/modules/bans.ts
@@ -27,25 +27,51 @@ bot.chatType("supergroup" || "group").command("ban", elevatedUsersOnly(async (ct
                     let ban_reason = ctx.match;
                     ban_message += `Reason: ${ban_reason}`;
                 }
-                await ctx.api.banChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id);
-                await ctx.reply(ban_message);
+                try {
+                  await ctx.api.banChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id);
+                }
+                catch (error) {
+                  await ctx.reply("Failed to ban user")
+                }
+                finally {
+                  await ctx.reply(ban_message);
+                }
+              }
             }
-        }
+ 
         else {
             if (ctx.match) {
                 const userids = await entityExtractor(ctx);
                 if (Object.keys(userids).length == 1) {
+                  try {
                     await ctx.api.banChatMember(ctx.chat.id, userids[0]);
-                    await ctx.reply("User has been banned!");
+                  }
+                  catch (error){
+                    await ctx.reply("Failed to ban user: Invalid user ID / username");
+                  }
+                  finally {
+                    await ctx.reply("User has been banned!")
+                  }
                 }
                 else if (Object.keys(userids).length > 1) {
                     await ctx.reply("Bulk banning users...")
                     for (let i = 0; i < userids.length; i++) {
+                      try {
                             await ctx.api.banChatMember(ctx.chat.id, userids[i]);
-                        }
+                      }
+                      catch (error) {
+                        // return for now
+                        return;
+                      }
+                      finally {
+                        // future pr: make object of banned users then print all those at once
+                        await ctx.reply("Users banned successfully!")
+                      }
+                      
+                    }
                 }
                 else {
-                    await ctx.reply("Failed to ban [!]", {reply_parameters: {message_id: ctx.message.message_id}});
+                    await ctx.reply("Failed to ban: Invalid or no user IDs / usernames!", {reply_parameters: {message_id: ctx.message.message_id}});
                     return;
                 }
             }
@@ -54,7 +80,6 @@ bot.chatType("supergroup" || "group").command("ban", elevatedUsersOnly(async (ct
                 await ctx.reply("Please type the user id or username next to /ban command or reply to a message with /ban command.", {reply_parameters: {message_id: ctx.message.message_id}});
                 return;
             }
-
         }
     }
 }));
