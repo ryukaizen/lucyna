4596dc342a0ade60828e94c0d7bb3da0973a9540
ryukaizen <ryukaizen@protonmail.com>
2024-03-09 19:43:50 +0530

    add tmute (temporary mute)

--- src/modules/mutes.ts
+++ src/modules/mutes.ts
@@ -284,3 +284,104 @@ bot.chatType("supergroup" || "group").command("unmute", elevatedUsersOnly(canRes
         }
     }
 })));
+
+bot.chatType("supergroup" || "group").command(["tmute", "tempmute"], elevatedUsersOnly(canRestrictUsers(async (ctx: any) => {
+    let user_info = await userInfo(ctx);
+    if (user_info.can_restrict_members == false) {
+        await ctx.reply("You don't have enough rights to mute users!", {reply_parameters: {message_id: ctx.message.message_id}});
+        return;
+    }
+    else {
+        if (ctx.message.reply_to_message != undefined) {
+            if (ctx.message.reply_to_message.from.id == bot.botInfo.id) {
+                await ctx.reply("YOU CAN'T MAKE ME STAY QUIET!!!", {reply_parameters: {message_id: ctx.message.message_id}});
+            }
+            else if (ctx.message.reply_to_message.from.id == ctx.from.id) {
+                await ctx.reply("You can just stop typing, you know?", {reply_parameters: {message_id: ctx.message.message_id}});
+            }
+            else if (await checkElevatedUser(ctx) == true) {
+                await ctx.reply("Muting the privileged users is out of my league :(", {reply_parameters: {message_id: ctx.message.message_id}});   
+            }
+            else {
+                let mute_message = (
+                    `<b>🔇 Temporarily muted</b> <a href="tg://user?id=${ctx.message.reply_to_message.from.id}">${ctx.message.reply_to_message.from.first_name}</a> (<code>${ctx.message.reply_to_message.from.id}</code>)<b>!</b>\n\n` +
+                    `Muted by: <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name}</a>\n` 
+                );
+                if (ctx.match) {
+                    let mute_duration = await extract_time(ctx, `${ctx.match}`);
+                    let converted_time = await convertUnixTime(Number(mute_duration));
+                    mute_message += `Duration: ${converted_time}`;
+
+                    await ctx.api.restrictChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id, mutePermissions, {until_date: mute_duration})
+                    .then(() => {
+                        ctx.api.sendMessage(ctx.chat.id, mute_message, {reply_markup: unmuteButton, parse_mode: "HTML"});
+                    })
+                    .catch((GrammyError: any) => {
+                        ctx.reply("Failed to mute user: invalid user / user probably does not exist.");
+                        logger.error(`${GrammyError}`);
+                        channel_log(`${GrammyError}\n\n` + `Timestamp: ${new Date().toLocaleString()}\n\n` + `Update object:\n${JSON.stringify(ctx.update,  null, 2)}`)
+                    });
+                }
+                else {
+                    await ctx.api.sendMessage(ctx.chat.id, "Please type the duration next to /tmute command, i.e /tmute <user's handle> 12h", {reply_parameters: {message_id: ctx.message.message_id}});
+                }
+            }
+        }
+        else {
+            let args = ctx.match;
+            if (args) {
+                let split_args = args.split(" ");
+                let user_id = split_args[0];
+                let user_info =  await ctx.getChatMember(user_id)
+                    .catch((GrammyError: any) => {
+                        return;
+                    });
+                if (user_info != undefined) {
+                    if (user_info.user.id == bot.botInfo.id) {
+                        await ctx.reply("YOU CAN'T MAKE ME STAY QUIET!!!", {reply_parameters: {message_id: ctx.message.message_id}});
+                        return;
+                    }
+                    else if (user_info.user.id == ctx.from.id) {
+                        await ctx.reply("You can just stop typing, you know?", {reply_parameters: {message_id: ctx.message.message_id}});
+                        return;
+                    }
+                    else if (await checkElevatedUserFrom(ctx, user_info) == true) {
+                        await ctx.reply("Muting the privileged users is out of my league :(", {reply_parameters: {message_id: ctx.message.message_id}});   
+                    }
+                    else {
+                        let mute_message = (
+                            `<b>🔇 Temporarily muted</b> <a href="tg://user?id=${user_info.user.id}">${user_info.user.first_name}</a> (<code>${user_info.user.id}</code>)<b>!</b>\n\n` +
+                            `Muted by: <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name}</a>\n` 
+                        );
+                        if (split_args[1] != undefined) {
+                            let mute_duration = await extract_time(ctx, `${split_args.slice(1).join(" ")}`);
+                            let converted_time = await convertUnixTime(Number(mute_duration));
+                            mute_message += `Duration: ${converted_time}`;
+                            await ctx.api.restrictChatMember(ctx.chat.id, user_info.user.id, mutePermissions, {until_date: mute_duration})
+                            .then(() => {
+                                ctx.api.sendMessage(ctx.chat.id, mute_message, {reply_markup: unmuteButton, parse_mode: "HTML"});
+                            })
+                            .catch((GrammyError: any) => {
+                                ctx.reply("Failed to mute user: invalid user / user probably does not exist.");
+                                logger.error(`${GrammyError}`);
+                                channel_log(`${GrammyError}\n\n` + `Timestamp: ${new Date().toLocaleString()}\n\n` + `Update object:\n${JSON.stringify(ctx.update,  null, 2)}`)
+                            });
+                        }
+                        else {
+                            await ctx.api.sendMessage(ctx.chat.id, "Please type the duration next to /tmute command, i.e /tmute <user's handle> 12h", {reply_parameters: {message_id: ctx.message.message_id}});
+                        }
+                    }
+                }
+                else {
+                    await ctx.reply("The provided user ID seems to be invalid!", {reply_parameters: {message_id: ctx.message.message_id}});
+                    return;
+                }
+            }       
+            else {        
+                await ctx.reply("Please type the user ID next to /mute command or reply to a message with /mute command.", {reply_parameters: {message_id: ctx.message.message_id}});
+                return;
+            }
+
+        }
+    }
+})));
