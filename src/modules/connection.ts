9504a3fd2060e4ec4d896fe775518aa45fec800c
ryukaizen <ryukaizen@protonmail.com>
2024-07-24 21:13:44 +0530

    feat: (CONNECTION) initial commit for connection module

---
+++ src/modules/connection.ts
@@ -0,0 +1,9 @@
+import { Composer } from "grammy";
+
+const composer = new Composer();
+
+composer.chatType(["supergroup", "group"]).command("connection", (async (ctx: any) => {
+    await ctx.reply("This feature is currently under development. Stay tuned - @Ryukaizen", {reply_to_message_id: ctx.message.message_id});
+}));
+
+export default composer;
