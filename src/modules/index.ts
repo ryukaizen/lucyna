e215ae0baa2b0240b46be0cabb011d62e930ade0
ryukaizen <ryukaizen@protonmail.com>
2024-07-24 21:15:25 +0530

    feat: import afk, antispam, disabling, connection, fbans, gbans, zombies modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,14 +1,20 @@
 import admin from './admin';
+import afk from './afk';
 import anilist from "./anilist";
 import antiflood from './antiflood';
+import antispam from './antispam';
 import bans from './bans';
 import blacklists from './blacklists'
 import devtools from './devtools'
+import disabling from './disabling';
 import extras from './extras';
 import cleanbluetext from './cleanbluetext';
 import cleanservice from './cleanservice'
+import connection from './connection';
+import fbans from './fbans';
 import filters from './filters';
 import fun from './fun';
+import gbans from './gbans';
 import locks from './locks';
 import misc from './misc';
 import mutes from './mutes';
@@ -21,6 +27,7 @@ import test from './test';
 import users from './users';
 import warns from './warns';
 import welcome from './welcome';
+import zombies from './zombies';
 
 import { Composer } from "grammy";
 
@@ -28,14 +35,20 @@ const composer = new Composer();
 
 composer.use(
     admin,
+    afk,
     anilist,
     antiflood,
+    antispam,
     bans,
     blacklists,
+    connection,
     devtools,
+    disabling,
     extras,
+    fbans,
     filters,
     fun,
+    gbans,
     locks,
     misc,
     mutes,
@@ -48,6 +61,7 @@ composer.use(
     users,
     warns,
     welcome,
+    zombies,
 // keep these at the very bottom to avoid conflicts with other modules (TODO: handle reply_parameters exception (400: Bad Request) when there is no message to reply)
     cleanservice,
     cleanbluetext, 
